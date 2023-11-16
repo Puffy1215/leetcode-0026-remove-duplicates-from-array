653aafee1a62c3e2e561fa22b05edd928e67ef7d
Andy Xie <andy.xie.opensource@gmail.com>
2023-11-16 20:57:23 +0800

    Initialize repo

---
+++ leetcode_0026_remove_duplicates_from_sorted_array/api.py
@@ -0,0 +1,12 @@
+"""API for solving problem Remove Duplicates from Sorted Array"""
+
+def _check_preconditions(...) -> bool:
+    pass
+
+
+def remove_duplicates_from_sorted_array(...) -> ...:
+    """Solves problem Remove Duplicates from Sorted Array"""
+
+    assert _check_preconditions(...)
+
+    pass
---
+++ tests/test_api.py
@@ -0,0 +1,18 @@
+"""Tests API for solving problem Remove Duplicates from Sorted Array"""
+
+import pytest
+
+from leetcode_0026_remove_duplicates_from_sorted_array import api
+
+
+@pytest.mark.parametrize(
+    ["result", ...],
+    (
+        [..., ...],
+        [..., ...],
+    )
+)
+def test_remove_duplicates_from_sorted_array(result, ...) -> None:
+    """Tests solution for problem Remove Duplicates from Sorted Array"""
+
+    assert api.remove_duplicates_from_sorted_array(...) == result
