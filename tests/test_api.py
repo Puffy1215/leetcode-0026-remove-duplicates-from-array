187a63b7bf9e37c6e2ad3c2f250e4bc9c6f3a405
Andy Xie <andy.xie.opensource@gmail.com>
2023-11-18 18:30:23 +0800

    test_api.py: Assert result

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,13 +6,17 @@ from leetcode_0026_remove_duplicates_from_sorted_array import api
 
 
 @pytest.mark.parametrize(
-    ["result", ...],
+    ["result", "nums"],
     (
-        [..., ...],
-        [..., ...],
-    )
+        [(2, [1, 2]), [1, 1, 2]],
+        [(5, [0, 1, 2, 3, 4]), [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]],
+    ),
 )
-def test_remove_duplicates_from_sorted_array(result, ...) -> None:
+def test_remove_duplicates_from_sorted_array(
+    result: tuple[int, list[int]], nums: list[int]
+) -> None:
     """Tests solution for problem Remove Duplicates from Sorted Array"""
 
-    assert api.remove_duplicates_from_sorted_array(...) == result
+    k = api.remove_duplicates_from_sorted_array(nums)
+
+    assert k, nums[:k] == result
