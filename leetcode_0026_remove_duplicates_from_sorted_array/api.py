f233fd56fb91a9c1e8911d7e6dc8930ec484c07a
Andy Xie <andy.xie.opensource@gmail.com>
2023-11-19 18:50:51 +0800

    add arg

--- leetcode_0026_remove_duplicates_from_sorted_array/api.py
+++ leetcode_0026_remove_duplicates_from_sorted_array/api.py
@@ -1,12 +1,13 @@
 """API for solving problem Remove Duplicates from Sorted Array"""
 
-def _check_preconditions(...) -> bool:
+
+def _check_preconditions(nums: list[int]) -> bool:
     pass
 
 
-def remove_duplicates_from_sorted_array(...) -> ...:
+def remove_duplicates_from_sorted_array(nums: list[int]) -> int:
     """Solves problem Remove Duplicates from Sorted Array"""
 
-    assert _check_preconditions(...)
+    assert _check_preconditions(nums)
 
     pass
